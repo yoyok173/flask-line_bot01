2fc4c2387203be3653a96934c5dcb71cbd8cd8d7
masataka <ar.tec.masa@gmail.com>
2018-03-24 00:39:04 +0900

    半角にも対応

--- main.py
+++ main.py
@@ -65,7 +65,11 @@ def callback():
 def message_text(event):
     if event.message.text == "買う！":
         text = "何を買うんですか？"
+    elif event.message.text == "買う!":
+        text = "何を買うんですか？"
     elif event.message.text == "買った！":
+        text = "何を買ったんですか？"
+    elif event.message.text == "買った!":
         text = "何を買ったんですか？"
     elif "ヘルプ" in event.message.text:
         text = "操作コマンド\n\n買う！\n＝＞何を買うんですか？\n買った！\n＝＞何を買ったんですか？\nおはよう　が含まれる\n＝＞おはようございます！\nそれ以外\n＝＞おうむ返し的なやつ"
