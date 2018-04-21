580bbb464aa69319e3c34b2d0a6a65f6c0e33e59
masataka <ar.tec.masa@gmail.com>
2018-04-21 23:58:15 +0900

    useridを取得する

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def callback():
 def message_text(event):
     if event.message.text == "買う！":
         text = "何を買うんですか？"
-        text1 = ""
+        text1 = event.source.userid
         # UserIdを一意にとる
         text = text + '\n\n' + 'あなたのLINEIDは\n' + text1
     elif event.message.text == "event":
