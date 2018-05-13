0c1e69fb9c3147a9315d2a74ba0513e26e90a503
masataka <ar.tec.masa@gmail.com>
2018-05-13 16:47:21 +0900

    リスト！などに対応

--- main.py
+++ main.py
@@ -205,7 +205,7 @@ def message_text(event):
     elif "おはよ" in event.message.text:
         text = "おはようございます！"
 
-    elif event.message.text == "リスト" or event.message.text == "りすと":
+    elif event.message.text == "リスト" or event.message.text == "りすと" or event.message.text == "りすと！" or event.message.text == "りすと!" or event.message.text == "リスト！" or event.message.text == "リスト!":
         text = "現在のお買い物リストです。"
         source_id = str(event.source.user_id)
         user_id = User.query.filter_by(source_id=source_id).first().id
